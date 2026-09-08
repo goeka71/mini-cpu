682aea257ab9e8d6f16cd5ff7cb9c892df802430
haedam <haedam0710@naver.com>
2026-09-09 00:08:49 +0900

    ISA 표 생성

--- src/adder.v
+++ src/adder.v
@@ -1,8 +1,8 @@
-module adder(
-  input  [3:0] a,
-  input  [3:0] b,
-  output [4:0] sum
-);
-  assign sum = a + b;
-endmodule
+module adder(a, b, sum);
+  input [3:0] a;
+  input [3:0] b;
+  output [4:0] sum;
 
+  assign sum = a + b;
+  
+endmodule
